7fbbd8b501dd1a318fbc432407451b0124675d9a
Manuel Riezebosch <mriezebosch@gmail.com>
2017-02-20 10:57:12 +0100

    file, fileinfo, directory, directoryinfo, path en streams

--- NETFOUNB/NETFOUNB.Tests/UnitTest1.cs
+++ NETFOUNB/NETFOUNB.Tests/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text;
 
 namespace NETFOUNB.Tests
 {
@@ -9,6 +11,87 @@ namespace NETFOUNB.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            File.WriteAllText("output.txt", "Goedemorgen allemaal.");
+            string text = File.ReadAllText("output.txt");
+
+            Assert.AreEqual("Goedemorgen allemaal.", text);
+        }
+
+        [TestMethod]
+        public void FileInfoClass()
+        {
+            var fi = new FileInfo("NETFOUNB.Tests.dll");
+
+            Assert.IsTrue(fi.Exists);
+            Assert.IsTrue(File.Exists("NETFOUNB.Tests.dll"));
+        }
+
+        [TestMethod]
+        public void DirectoryInfoClassDemo()
+        {
+            var di = new DirectoryInfo($@"C:\git");
+            di.GetFiles("*.*", SearchOption.AllDirectories);
+
+            di.GetFiles();
+        }
+
+        [TestMethod]
+        public void PathClassDemo()
+        {
+            
+            Path.Combine(@"C:\git", "output.log");
+            var path = Path.ChangeExtension("input.zip", "bak");
+
+            Assert.AreEqual("input.bak", path);
+        }
+
+        [TestMethod]
+        public void RenameVanFileDemo()
+        {
+            var fi = new FileInfo("original.txt");
+            File.WriteAllText(fi.FullName, "testje");
+
+            Assert.IsTrue(fi.Exists);
+
+            var path = Path.ChangeExtension(fi.FullName, "log");
+            File.Delete(path);
+            fi.MoveTo(path);
+
+            Assert.IsTrue(fi.Exists);
+            Assert.AreEqual("original.log", fi.Name);
+        }
+
+        [TestMethod]
+        public void StreamsDemo()
+        {
+            string path = "stream-demo.txt";
+
+            File.Delete(path);
+
+            var stream = File.OpenWrite(path);
+            using (var writer = new StreamWriter(stream))
+            {
+                writer.Write("hoi");
+                throw new Exception();
+            }
+        }
+
+        [TestMethod]
+        public void MemoryStreamDemo()
+        {
+            var stream = new MemoryStream();
+            using (var writer = new StreamWriter(stream, Encoding.UTF8, 1024, true))
+            {
+                writer.Write("hallo memory");
+            }
+
+            stream.Seek(0, SeekOrigin.Begin);
+
+            using (var reader = new StreamReader(stream))
+            {
+                var text = reader.ReadToEnd();
+                Assert.AreEqual("hallo memory", text);
+            }
         }
     }
 }
