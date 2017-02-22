ea0f27c4758e8ad671050eb23011375c8250617a
Manuel Riezebosch <mriezebosch@gmail.com>
2017-02-22 09:19:43 +0100

    process class demo

---
+++ NETFOUNB/NETFOUNB.Tests/MonitoringProcesses.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Linq;
+using System.IO;
+
+namespace NETFOUNB.Tests
+{
+    [TestClass]
+    public class MonitoringProcesses
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var all = Process.GetProcesses();
+            Console.WriteLine(string.Join(", ", all.Select(p => p.ProcessName)));
+        }
+
+        [TestMethod]
+        public void KillProcess()
+        {
+            Process
+                .GetProcessesByName("chrome")
+                .ToList()
+                .ForEach(p => p.Kill());
+        }
+
+        [TestMethod]
+        public void StartProcess()
+        {
+            var path = Guid.NewGuid().ToString();
+            path = Path.ChangeExtension(path, "txt");
+
+            string original = "Dit moet je aanpassen.";
+            File.WriteAllText(path, original);
+
+            var p = Process.Start("notepad", path);
+            p.WaitForExit();
+
+            var text = File.ReadAllText(path);
+            Assert.AreNotEqual(text, original, "De tekst is niet aangepast");
+        }
+    }
+}
---
+++ NETFOUNB/PerformanceCountersDemo/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerformanceCountersDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreatePerformanceCounters();
+            var counters = RetrieveCounters();
+
+            ExecuteUntilQuit(counters);
+        }
+
+        private static void ExecuteUntilQuit(Dictionary<string, PerformanceCounter> counters)
+        {
+            var key = Console.ReadKey();
+            while (key.Key != ConsoleKey.Q)
+            {
+                PerformanceCounter counter;
+                if (!counters.TryGetValue(key.Key.ToString(), out counter))
+                {
+                    counter = counters["#"];
+                }
+
+                counter.IncrementBy(10);
+                key = Console.ReadKey();
+            }
+        }
+
+        private static Dictionary<string, PerformanceCounter> RetrieveCounters()
+        {
+            var category = PerformanceCounterCategory
+                            .GetCategories()
+                            .First(pc => pc.CategoryName == "demo");
+
+            var counters = category
+                .GetCounters()
+                .ToDictionary(c => c.CounterName.ToUpper());
+
+            counters
+                .Values
+                .ToList()
+                .ForEach(c => c.ReadOnly = false);
+
+            return counters;
+        }
+
+        private static void CreatePerformanceCounters()
+        {
+            if (!PerformanceCounterCategory.Exists("demo"))
+            {
+                var collection = new CounterCreationDataCollection();
+                collection.Add(new CounterCreationData("A", "Key A was pressed", PerformanceCounterType.SampleCounter));
+                collection.Add(new CounterCreationData("S", "Key S was pressed", PerformanceCounterType.SampleCounter));
+                collection.Add(new CounterCreationData("D", "Key D was pressed", PerformanceCounterType.SampleCounter));
+                collection.Add(new CounterCreationData("F", "Key F was pressed", PerformanceCounterType.SampleCounter));
+                collection.Add(new CounterCreationData("#", "Another key was pressed", PerformanceCounterType.SampleCounter));
+
+                PerformanceCounterCategory.Create("demo", "demo for performance counters", PerformanceCounterCategoryType.SingleInstance, collection);
+            }
+        }
+    }
+}
